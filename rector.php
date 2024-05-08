80a4bc7d88a15bf1f715b469def0a30a38340863
auooru <auooru@outlook.com>
2024-05-08 10:54:01 +0800

    update: 更新开发依赖

--- rector.php
+++ rector.php
@@ -6,28 +6,19 @@ use Rector\Config\RectorConfig;
 use Rector\Php70\Rector\FuncCall\RandomFunctionRector;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/config',
-    ]);
-    $rectorConfig->bootstrapFiles([]);
-
-    $rectorConfig->skip([
+    ])
+    ->withSkip([
         JsonThrowOnErrorRector::class,
         AddLiteralSeparatorToNumberRector::class,
         // 暂不需要安全随机数
         RandomFunctionRector::class
+    ])
+    ->withPhpSets()
+    ->withPHPStanConfigs([
+        __DIR__ . '/phpstan.neon',
     ]);
-
-    $rectorConfig->rules([]);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_74,
-    ]);
-
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
-};
